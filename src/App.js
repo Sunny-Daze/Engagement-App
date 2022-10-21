7c5db71dfeffd7d77ffd66dcd2385102a016d83c
Sunny-Daze7 <sunnyadav0727@gmail.com>
2022-10-21 05:38:00 +0530

    navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
-import {React} from "react";
-import Sidebar from "./components/sideBar";
+import "./App.css";
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Community from "./pages/Community"
+import Projects from "./pages/Projects"
+import Training from "./pages/Training"
+import Recreation from "./pages/Recreation"
+import Management from "./pages/Management"
+import Store from "./pages/Store"
+import Sidebar from "./components/Sidebar";
 
 function App() {
-  return (
-    <>
-    <Sidebar />
-    </>
-  );
+return (
+	<>
+	<Router>
+		<Sidebar>
+		<Routes>
+			<Route path="/" element={<Community />} />
+			<Route path="/projects" element={<Projects />} /> 
+			<Route path="/training" element={<Training />} />
+			<Route path="/recreation" element={<Recreation />} />
+			<Route path="/management" element={<Management />} />
+			<Route path="/store" element={<Store />} />
+		</Routes>
+		</Sidebar>
+	</Router>
+	</>
+);
 }
 
 export default App;
--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,10 +1,73 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { NavLink } from 'react-router-dom'
+import { motion } from "framer-motion"
+import { FaRegCompass, FaBars } from "react-icons/fa"
+import { MdOutlineAccountTree } from "react-icons/md"
+import { BsLightningChargeFill, BsPlay } from "react-icons/bs"
+import { FiUsers } from "react-icons/fi"
+import { HiShoppingCart } from "react-icons/hi"
 
-function Sidebar() {
+const routes = [
+    {
+        path : "/",
+        name : "Community",
+        icon : <FaRegCompass />
+    },
+    {
+        path : "/projects",
+        name : "Projects",
+        icon : <MdOutlineAccountTree />
+    },
+    {
+        path : "/training",
+        name : "Training",
+        icon : <BsLightningChargeFill />
+    },
+    {
+        path : "/recreation",
+        name : "Recreation",
+        icon : <BsPlay />
+    },
+    {
+        path : "/management",
+        name : "Management",
+        icon : <FiUsers />
+    },
+    {
+        path : "/store",
+        name : "Store",
+        icon : <HiShoppingCart />
+    }
+]
+
+const Sidebar = ({ children }) => {
+    const [isOpen, setIsOpen] = useState(0)
+
+    const toggle = () => setIsOpen(!isOpen)
   return (
-    <>
-    <h1>hello</h1>
-    </>
+    <div className='main-container'>
+        <motion.div animate={{ width : "200px"}} className="side-bar">
+
+            <div className="top-section">
+                <h1 className='logo'>Ngage</h1>
+                <div className='bars'>
+                    <FaBars />
+                </div>
+            </div>
+
+            <section className='routes'>
+                {routes.map((route) => (
+                    <NavLink to={route.path} key={route.name} className="link">
+                        <div className='icon'>{route.icon}</div>
+                        <div className='link-text'>{route.name}</div>
+                    </NavLink>
+                ))}
+            </section>
+        </motion.div>
+        <main>
+            {children}
+        </main>
+    </div>
   )
 }
 
